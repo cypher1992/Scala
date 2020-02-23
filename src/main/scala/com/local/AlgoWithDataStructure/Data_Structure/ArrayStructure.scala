0fa2655f25b08e5ada84c26272757827e3877063
Raymond Yassin <ryassin92@gmail.com>
2020-02-23 18:55:49 -0500

    AlgowithDataStructure:Array Add: mergeArray() with Test

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/ArrayStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/ArrayStructure.scala
@@ -1,14 +1,26 @@
 package com.local.AlgoWithDataStructure.Data_Structure
-
+import scala.collection.mutable.ArrayBuffer
 class ArrayStructure{
   /*
     Arrays follow O(1) in the Asymptotic
     Arrays are linear data structure
     Arrays start at 0 and end at n-1; n represents the size of the array
+    Array doe not grow in size after initialization so we use ArrayBuffer
+
+    URL:https://alvinalexander.com/scala/scala-mutable-arrays-adding-elements-to-arrays
+    Obj: Array vs ArrayBuffer
+    Info: Just a quick note today that if you want to create a mutable Scala array — particularly an array that can grow in size after you first declare it — you need to use the Scala ArrayBuffer class instead of the Array class
   */
-  def multiply2(array:Array[Int]):Array[Int] ={
+  def multiply2(array:ArrayBuffer[Int]):ArrayBuffer[Int] ={
     array match{
-      case a if(array.isInstanceOf[Array[Int]]) => array.map(_ * 2)
+      case a if(array.isInstanceOf[ArrayBuffer[Int]]) => array.map(_ * 2)
+    }
+  }
+
+  def mergeArray[T](array:ArrayBuffer[T],array2:ArrayBuffer[T]):ArrayBuffer[T] ={
+    array2.size match {
+      case 0 => array
+      case _ => mergeArray(array :+ array2.head,array2.tail)
     }
   }
 }
--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/ArrayTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/ArrayTest.scala
@@ -2,6 +2,7 @@ package com.AlgoAndDataStructureTest.ArrayStructureTest
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
+import scala.collection.mutable.ArrayBuffer
 import org.scalatest.Assertions._
 import com.local.AlgoWithDataStructure.Data_Structure.ArrayStructure
 
@@ -9,11 +10,37 @@ class ArrayTest extends AnyFlatSpec with Matchers {
   val arrayObj:ArrayStructure = new ArrayStructure
 
   "AlgoAndDataStructureTest: multiply2(Arry(0...5))" should "return Array(0,2,4,6,8,10)" in {
-    val array:Array[Int] = Array.range(0,6)
-    val actual:Array[Int] = arrayObj.multiply2(array)
-    val expected:Array[Int]= Array(0,2,4,6,8,10)
+    val array:ArrayBuffer[Int] = ArrayBuffer.range(0,6)
+    val actual:ArrayBuffer[Int] = arrayObj.multiply2(array)
+    val expected:ArrayBuffer[Int]= ArrayBuffer(0,2,4,6,8,10)
 
+
+    /*
+    == vs ===
+    Equals with == refers to the object location interms of hash
+    Equals with === refers to the object context are the same
+    */
     assert( actual === expected)
+  }
+
+  "AlgoAndDataStructureTest: mergeArrays(Arry(0...5),Array.Empty[Int]" should "return ArrayBuffer(0,1,2,3,4,5)" in {
+
+    val array:ArrayBuffer[Int] = ArrayBuffer.range(0,6)
+    val array2:ArrayBuffer[Int] = ArrayBuffer.empty[Int]
+    val actual:ArrayBuffer[Int] = arrayObj.mergeArray(array,array2)
+    val expected:ArrayBuffer[Int] = ArrayBuffer(0,1,2,3,4,5)
+    assert( actual === expected)
+
+  }
+  
+  "AlgoAndDataStructureTest: mergeArrays(Arry(0...5),Array(0..3))" should "return ArrayBuffer(0,1,2,3,4,5,0,1,2,3)" in {
+
+    val array:ArrayBuffer[Int] = ArrayBuffer.range(0,6)
+    val array2:ArrayBuffer[Int] = ArrayBuffer.range(0,4)
+    val actual:ArrayBuffer[Int] = arrayObj.mergeArray(array,array2)
+    val expected:ArrayBuffer[Int] = ArrayBuffer(0,1,2,3,4,5,0,1,2,3)
+    assert( actual === expected)
+
   }
 
 }

00cc1e99bd985b37292eae394313f161919b48a1
Raymond Yassin <ryassin92@gmail.com>
2020-02-26 21:11:22 -0500

    AlgoAndDataStructure List: remove() with test

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/ListTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/ListTest.scala
@@ -8,7 +8,7 @@ class ListTest extends AnyFlatSpec  with Matchers {
 
   val listClass:ListStructure = new ListStructure
 
-  "AlgoAndDataStrucutre List: add(5,List.empty[Int])" should "return List(5)" in{
+  "AlgoAndDataStructure List: add(5,List.empty[Int])" should "return List(5)" in{
 
     val index:Int = 5
     val emptyList:List[Int] = List.empty[Int]
@@ -19,7 +19,7 @@ class ListTest extends AnyFlatSpec  with Matchers {
 
   }
 
-  "AlgoAndDataStrucutre List: add(5,List(5,6,5))" should "return List(5,6,5,5)" in{
+  "AlgoAndDataStructure List: add(5,List(5,6,5))" should "return List(5,6,5,5)" in{
 
     val index:Int = 5
     val list:List[Int] = List(5,6,5)
@@ -30,4 +30,26 @@ class ListTest extends AnyFlatSpec  with Matchers {
 
   }
 
+  "AlgoAndDataStructure List: remove(5,List())" should "return List()" in{
+
+    val index:Int = 5
+    val list:List[Int] = List.empty[Int]
+    val actual:List[Int] = listClass.remove(index,list)
+    val expected:List[Int] = List.empty[Int]
+
+    assert(actual == expected)
+
+  }
+
+  "AlgoAndDataStructure List: remove(5,List(5,5,6,8,9,5))" should "return List(6,8,9)" in{
+
+    val index:Int = 5
+    val list:List[Int] = List(5,5,6,8,9,5)
+    val actual:List[Int] = listClass.remove(index,list)
+    val expected:List[Int] = List(6,8,9)
+
+    assert(actual == expected)
+
+  }
+  
 }

7c4846f2f8f60d30c38fd1ac1faf94b7634f5fb6
Raymond Yassin <ryassin92@gmail.com>
2020-03-02 20:06:28 -0500

    AlgoAndDataStructure Vector: Add findVector() and findLambdaVector()

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/VectorStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/VectorStructure.scala
@@ -6,4 +6,13 @@ class VectorStructure{
 
   def appendVector[T](vec:Vector[T],value:T):Vector[T] = vec :+ value
 
+  def findLamdaVector[T](query:T,vector:Vector[T]):Option[T] = vector.find(x => x == query)
+
+  def findVector[T](query:T,vector:Vector[T]):Option[T] = {
+    vector match {
+      case a if(vector.isEmpty) => None
+      case b if(vector.head == query) => Some(vector.head)
+      case _ => findVector(query,vector.tail)   
+    }
+  }
 }

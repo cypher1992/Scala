3b42614cc55e9f53c275990cee8f3be41ff6481b
Raymond Yassin <ryassin92@gmail.com>
2020-03-09 21:49:54 -0400

    FutureThread: futureUnWrap(query()) returns succeed

--- src/main/scala/com/local/FutureThreads/MockDAO.scala
+++ src/main/scala/com/local/FutureThreads/MockDAO.scala
@@ -2,6 +2,7 @@ package com.local.FutureThreads
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 class MockDAO {
 
@@ -14,5 +15,14 @@ class MockDAO {
       }
     }
     queryAppend()
+  }
+
+  // using Success and failure to return  object from a Future
+  def futureUnWrap[T](future:Future[T]):Unit ={
+    future.onComplete{
+      case Success(value)=> println(s"""${value}""")
+      case Failure(e) => println(s"""${e}""")
+    }
+
   }
 }
--- src/test/scala/com/AllAboutScalaExcerciseTDD/FuturesTesting.scala
+++ src/test/scala/com/AllAboutScalaExcerciseTDD/FuturesTesting.scala
@@ -5,18 +5,25 @@ import com.local.FutureThreads.MockDAO
 import scala.language.postfixOps
 import scala.concurrent.duration._
 import scala.concurrent.{Await}
+import org.scalatest.concurrent.ScalaFutures._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Success
+import scala.util.{Success,Failure}
 
 class FuturesTesting extends AnyFlatSpec with Matchers{
 
   val mockDao:MockDAO= new MockDAO
 
-  "FutureThreads MockDao: query()" should "return Future[List[Int]]" in {
+  "FutureThreads MockDao: query()" should "return List[Int]" in {
     val actual:List[Int] = Await.result(mockDao.query(),5 seconds)
     val expected:List[Int] = List.range(30,0,-1)
 
     assert(actual === expected)
+  }
+
+  "FutureThread MockDao: futureUnWrap(query())" should "succeed" in {
+    mockDao.futureUnWrap(mockDao.query())
+    // validate if future object was a success or not
+    succeed
   }
 
 }

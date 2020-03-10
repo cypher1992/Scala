01780e56622df0bb953054308ddabd9646ed5887
Raymond Yassin <ryassin92@gmail.com>
2020-03-09 21:07:10 -0400

    FutureThread: add querytest returns list[Int] from a future

--- src/test/scala/com/AllAboutScalaExcerciseTDD/FuturesTesting.scala
+++ src/test/scala/com/AllAboutScalaExcerciseTDD/FuturesTesting.scala
@@ -2,8 +2,21 @@ package com.AllAboutScalaExcerciseTDD
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import com.local.FutureThreads.MockDAO
+import scala.language.postfixOps
+import scala.concurrent.duration._
+import scala.concurrent.{Await}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
 
 class FuturesTesting extends AnyFlatSpec with Matchers{
-  
+
+  val mockDao:MockDAO= new MockDAO
+
+  "FutureThreads MockDao: query()" should "return Future[List[Int]]" in {
+    val actual:List[Int] = Await.result(mockDao.query(),5 seconds)
+    val expected:List[Int] = List.range(30,0,-1)
+
+    assert(actual === expected)
+  }
 
 }

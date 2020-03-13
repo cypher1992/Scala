c69a1dca18fc63194942791a5a0cfc78534627a1
Raymond Yassin <ryassin92@gmail.com>
2020-03-12 22:39:41 -0400

    Refactor AlgoAndDataStructure:  EagerVLazy test with Memory Compare

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/Concept/EagarVLazyTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/Concept/EagarVLazyTest.scala
@@ -3,26 +3,27 @@ package com.AlgoAndDataStructureTest.ArrayStructureTest.Concept
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import com.local.AlgoWithDataStructure.Data_Structure.Concepts.EagerVLazy
+import java.lang.Runtime
 
 class EagarVLazyTest extends AnyFlatSpec with Matchers {
 
   val evl:EagerVLazy = new EagerVLazy()
-
+  val rt = Runtime.getRuntime
   "AlgoAndDataStrucutures Concept: EagerVLazy: testing val lazy " should "return not instantiated" in{
 
-    println(
-      s"""
-        |${evl.lazyList.getClass}
-      """.stripMargin)
+    evl.lazyList.getClass
+    val lazyMemory:Long = rt.totalMemory() - rt.freeMemory()
+    val eagerMemory: Long = 58754616
+    assert(lazyMemory < eagerMemory)
 
   }
 
   "AlgoAndDataStrucutures Concept: EagerVLazy: testing val eager " should "return eager" in{
 
-    println(
-      s"""
-         |${evl.eagerList.getClass}
-      """.stripMargin)
+    evl.eagerList.getClass
+    val eagerMemory:Long = rt.totalMemory() - rt.freeMemory()
+    val lazyMemory: Long = 58754616
+    assert(lazyMemory < eagerMemory)
 
   }
 

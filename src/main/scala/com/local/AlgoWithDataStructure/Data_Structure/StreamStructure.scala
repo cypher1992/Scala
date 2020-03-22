cc8a218913daa242f648fb09184368f02c394199
Raymond Yassin <ryassin92@gmail.com>
2020-03-21 20:59:53 -0400

    AlgoAndDataStructure Stream: refactor appendLambdaStream() and appendStream()

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/StreamStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/StreamStructure.scala
@@ -2,9 +2,15 @@ package com.local.AlgoWithDataStructure.Data_Structure
 
 import scala.collection.immutable.Stream
 
+/*
+  Notes:
+   - Stream is deprecated.
+   - Stream implements both lazy and memoization
+*/
+
 class StreamStructure {
 
-  def appendStream[T](stream:Stream[T],add:T):Stream[T] = stream.appended(add)
+  def appendLambdaStream[T](stream:Stream[T],add:T):Stream[T] = stream.appended(add)
+  def appendStream[T](stream:Stream[T],add:T):Stream[T] = stream :+ add
 
-  
 }

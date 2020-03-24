06d3ec38988cdef7be3c68bc6a8322baac8b0295
Raymond Yassin <ryassin92@gmail.com>
2020-03-23 21:58:05 -0400

    AlgoAndDataStrucuture Stream: Add findStream()

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/StreamStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/StreamStructure.scala
@@ -13,4 +13,12 @@ class StreamStructure {
   def appendLambdaStream[T](stream:Stream[T],add:T):Stream[T] = stream.appended(add)
   def appendStream[T](stream:Stream[T],add:T):Stream[T] = stream :+ add
 
+  def findStream[T](value:T,stream:Stream[T]):Option[T] ={
+    stream match {
+      case a if(stream.isEmpty) => None
+      case b if(stream.head == value) => Some(stream.head)
+      case _ => findStream(value,stream.tail)
+    }
+  }
+
 }

04fd57ba9a3a65113026a21f44fbdc3b25865bcb
Raymond Yassin <ryassin92@gmail.com>
2020-04-10 12:52:11 -0400

    AlgoAndDataStrucuture Algo: Add Class SelectionSort(list)

---
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/SelectionSort.scala
@@ -0,0 +1,15 @@
+package com.local.AlgoWithDataStructure.Data_Structure.Algo
+
+class SelectionSort(private val list:List[Int]) {
+
+  def getList():List[Int] = {
+    this.list
+  }
+
+  def setList(newList:List[Int]):List[Int]={
+    val this.list = newList
+    this.list
+  }
+
+
+}
---
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/SelectionSortTest.scala
@@ -0,0 +1,9 @@
+package com.AlgoAndDataStructureTest.ArrayStructureTest.AlgoTest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import com.local.AlgoWithDataStructure.Data_Structure.Algo.SelectionSort
+
+class SelectionSortTest extends AnyFlatSpec with Matchers {
+  
+}

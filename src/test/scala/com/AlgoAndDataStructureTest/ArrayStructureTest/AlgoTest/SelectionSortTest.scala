be39b92a3c525539bbed03a1879a5b25ef4d4651
Raymond Yassin <ryassin92@gmail.com>
2020-04-10 12:53:18 -0400

    AlgoAndDataStrucuture SelectionSort: Add test getList()

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/SelectionSortTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/SelectionSortTest.scala
@@ -5,5 +5,13 @@ import org.scalatest.matchers.must.Matchers
 import com.local.AlgoWithDataStructure.Data_Structure.Algo.SelectionSort
 
 class SelectionSortTest extends AnyFlatSpec with Matchers {
-  
+
+  "AlgoAndDataStructure: SelectionSort def getList()" should "return list" in {
+    val list:List[Int] = List(3,2,1)
+    val ss:SelectionSort = new SelectionSort(list)
+    val actual:List[Int] = ss.getList()
+    val expected:List[Int] = List(3,2,1)
+    assert(expected == actual)
+  }
+
 }

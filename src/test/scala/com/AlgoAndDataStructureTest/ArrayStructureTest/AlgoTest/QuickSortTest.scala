246eb42dacc26b22ff05ed69f5160363899d1bc5
Raymond Yassin <ryassin92@gmail.com>
2020-04-13 21:52:47 -0400

    AlgoAndDataStructure Quicksort: Test setArray(newArray)

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/QuickSortTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/QuickSortTest.scala
@@ -16,5 +16,14 @@ class QuickSortTest extends AnyFlatSpec with Matchers {
 
   }
 
-
+  "AlgoAndDataStructure QuickSort: qs.setArray(NewArray" should "return NewArray" in{
+    val array:Array[Int] = Array.empty[Int]
+    val newArray:Array[Int] = Array(1,34,663,36,2,324,68,9)
+    val qs:QuickSort = new QuickSort(array)
+    qs.setArray(newArray)
+    val actual:Array[Int] = qs.getArray()
+    val expected: Array[Int] = Array(1,34,663,36,2,324,68,9)
+    assert(actual === expected)
+  }
+  
 }

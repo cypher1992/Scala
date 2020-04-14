a2c5f0d8197e9f00a78718553473fa89174cee25
Raymond Yassin <ryassin92@gmail.com>
2020-04-13 21:36:28 -0400

    AlgoAndDataStructure Quicksort: Add class and getters and setter function

---
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/QuickSort.scala
@@ -0,0 +1,11 @@
+package com.local.AlgoWithDataStructure.Data_Structure.Algo
+
+class QuickSort(private var array:Array[Int]){
+
+  def getArray():Array[Int] = this.array
+  def setArray(newArray:Array[Int]):Array[Int] = {
+    this.array = newArray
+    this.array
+  }
+
+}

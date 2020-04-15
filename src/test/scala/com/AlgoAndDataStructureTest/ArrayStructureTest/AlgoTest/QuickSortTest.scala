999b7948cbbeb0d19dbc78bc445cbe2eda3e8def
Raymond Yassin <ryassin92@gmail.com>
2020-04-14 22:05:33 -0400

    AlgoAndDataStructure Quicksort: Test getList()

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/QuickSortTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/QuickSortTest.scala
@@ -6,11 +6,11 @@ import com.local.AlgoWithDataStructure.Data_Structure.Algo.QuickSort
 
 class QuickSortTest extends AnyFlatSpec with Matchers {
 
-  "AlgoAndDataStructure QuickSort: qs.getArray()" should "return Array" in {
-      val array:Array[Int] = Array(4,51,6,2,77,3)
-      val qs:QuickSort = new QuickSort(array)
-      val actual:Array[Int] = qs.getArray()
-      val expected:Array[Int] = Array(4,51,6,2,77,3)
+  "AlgoAndDataStructure QuickSort: qs.getList()" should "return List" in {
+      val lst:List[Int] = List(4,51,6,2,77,3)
+      val qs:QuickSort = new QuickSort(lst)
+      val actual:List[Int] = qs.getList()
+      val expected:List[Int] = List(4,51,6,2,77,3)
 
       assert(expected === actual)
 

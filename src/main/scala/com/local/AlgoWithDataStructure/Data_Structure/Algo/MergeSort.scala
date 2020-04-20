b45a7b546068e6d3ea576ccfb384b5bf9a1148fe
Raymond Yassin <ryassin92@gmail.com>
2020-04-20 19:30:08 -0400

    AlgoAndDataStructure MergeSort: add mergeSort() and merge()

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/MergeSort.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/MergeSort.scala
@@ -5,4 +5,23 @@ class MergeSort(var list:List[Int]) {
     def getList():List[Int] = this.list
     def setList(newlist:List[Int]):Unit = this.list = newlist
 
+    def merge(l1:List[Int],l2:List[Int]):List[Int] ={
+      (l1,l2) match{
+        case (Nil, _) => l2
+        case (_,Nil) =>l1
+        case (h1::t1, h2::t2) => if(h1<h2) h1::merge(t1,l2)
+        else h2::merge(l1,t2)
+      }
+    }
+
+    def mergeSort(list:List[Int]):List[Int] ={
+      list match {
+        case Nil => list
+        case h::Nil => list
+        case _ =>
+            val (l1,l2) = list.splitAt(list.length/2)
+            merge(mergeSort(l1),mergeSort(l2))
+
+      }
+    }
 }

1198ba6e90590415cd205f863b63d054b2578cdf
Raymond Yassin <ryassin92@gmail.com>
2020-04-19 22:25:59 -0400

    Scala - AlgoAndDataStructure MergeSort: Add class MergeSort with getters and setters

---
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/MergeSort.scala
@@ -0,0 +1,8 @@
+package com.local.AlgoWithDataStructure.Data_Structure.Algo
+
+class MergeSort(var list:List[Int]) {
+
+    def getList():List[Int] = this.list
+    def setList(newlist:List[Int]):Unit = this.list = newlist
+
+}

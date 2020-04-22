51de176f61ee75ddae505eb79f9b9b80842c66e9
Raymond Yassin <ryassin92@gmail.com>
2020-04-21 23:07:26 -0400

    AlgoAndDataStructure Bubblesort: add class and getter and setters for list

---
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/BubbleSort.scala
@@ -0,0 +1,7 @@
+package com.local.AlgoWithDataStructure.Data_Structure.Algo
+
+class BubbleSort (var list:List[Int]) {
+  def getList():List[Int] = this.list
+  def setList(newlist:List[Int]):Unit = this.list = newlist
+  
+}

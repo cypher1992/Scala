7ca996b11ce220d8acab40b85ab6d483bba22ce6
Raymond Yassin <ryassin92@gmail.com>
2020-04-22 22:57:43 -0400

    AlgoAndDataStructure Bubblesort: add bubbleSort() and refactor  method to accept arrays

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/BubbleSort.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/BubbleSort.scala
@@ -1,7 +1,20 @@
 package com.local.AlgoWithDataStructure.Data_Structure.Algo
 
-class BubbleSort (var list:List[Int]) {
-  def getList():List[Int] = this.list
-  def setList(newlist:List[Int]):Unit = this.list = newlist
+class BubbleSort (var array:Array[Int]) {
+  def getArray():Array[Int] = this.array
+  def setArray(newArray:Array[Int]):Unit = this.array = newArray
+
+  def bubbleSort(array:Array[Int] = this.getArray()):Array[Int]={
+    for(i <- 0 until array.length-1){
+      for(j <- 0 until array.length-1-i){
+        if(array(j) >array(j+1)){
+          val temp:Int = array(j)
+          array(j) = array(j+1)
+          array(j+1) = temp
+        }
+      }
+    }
+    array
+  }
 
 }

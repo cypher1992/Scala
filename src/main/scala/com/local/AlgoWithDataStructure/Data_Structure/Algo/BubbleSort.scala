4dc4300fd0548860ffaef656efdd8de0709dcd88
Raymond Yassin <ryassin92@gmail.com>
2020-04-22 20:35:49 -0400

    AlgoAndDataStructure Bubblesort: test getList() returns list

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/BubbleSort.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Algo/BubbleSort.scala
@@ -3,5 +3,5 @@ package com.local.AlgoWithDataStructure.Data_Structure.Algo
 class BubbleSort (var list:List[Int]) {
   def getList():List[Int] = this.list
   def setList(newlist:List[Int]):Unit = this.list = newlist
-  
+
 }
---
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/AlgoTest/BubbleSortTest.scala
@@ -0,0 +1,19 @@
+package com.AlgoAndDataStructureTest.ArrayStructureTest.AlgoTest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import com.local.AlgoWithDataStructure.Data_Structure.Algo.BubbleSort
+
+class BubbleSortTest extends AnyFlatSpec  with Matchers {
+
+  "AlgoAndDataStructure BubbleSort: getList()" should "return list" in {
+    val emptyList:List[Int] = List.empty[Int]
+    val bs:BubbleSort = new BubbleSort(emptyList)
+    val expected:List[Int] = List.empty[Int]
+    val actual:List[Int] = bs.getList()
+
+    assert(expected === actual)
+  }
+
+
+}

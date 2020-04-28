65b94025ee8ef2a6710bd65c1415133d65c205f0
Raymond Yassin <ryassin92@gmail.com>
2020-04-27 20:21:00 -0400

    AlgoAndDataStructure Stack: refactor test stack.push(1) returns array(1,1,0)

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/Concept/StackTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/Concept/StackTest.scala
@@ -20,7 +20,7 @@ class StackTest extends AnyFlatSpec with Matchers{
     assert(actual === expected)
   }
 
-  "AlgoAndDataStructure StackStructure: stack.push(1)" should "returns Array(1)" in {
+  "AlgoAndDataStructure StackStructure: stack.push(1)" should "returns Array(1,0,0)" in {
     val stack: StackStructure = new StackStructure(3)
     stack.push(1)
     val actual:Array[Int] = stack.getStackBox()
@@ -28,4 +28,13 @@ class StackTest extends AnyFlatSpec with Matchers{
     assert(actual === expected)
   }
   
+  "AlgoAndDataStructure StackStructure: stack.push(1)" should "returns Array(1,1,0)" in {
+    val stack: StackStructure = new StackStructure(3)
+    stack.push(1)
+    stack.push(1)
+    val actual:Array[Int] = stack.getStackBox()
+    val expected:Array[Int] = Array(1,1,0)
+    assert(actual === expected)
   }
+
+}

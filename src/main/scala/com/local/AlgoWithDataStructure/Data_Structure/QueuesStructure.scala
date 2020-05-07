054ec9ebaeb1425d1e08821067e5542a6ef472e9
Raymond Yassin <ryassin92@gmail.com>
2020-05-06 21:39:09 -0400

    AlgoAndDataStructure Queue: add insert(data) append data to rear of queue

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/QueuesStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/QueuesStructure.scala
@@ -3,7 +3,19 @@ package com.local.AlgoWithDataStructure.Data_Structure
 class QueueStructure(maxSize:Int) {
     //FIFO
     private var queueBox:Array[Any] = new Array[Any](maxSize)
+    private var front:Int = 0
+    private var rear:Int = -1
+    private var numOfItems:Int = 0
 
     def getQueueBox():Array[Any] = this.queueBox
+
+    def insert(data:Any):Unit ={
+        if(rear == maxSize-1){
+            this.rear = -1
+        }
+        this.rear += 1
+        this.queueBox(rear) = data
+        this.numOfItems +=1
+    }
 
 }

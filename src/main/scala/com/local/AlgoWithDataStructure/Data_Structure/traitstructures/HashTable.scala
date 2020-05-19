0c1315bfcd814db73912110e4698bdc63d062e84
Raymond Yassin <ryassin92@gmail.com>
2020-05-18 22:59:25 -0400

    AlgoWithDataStructure HashTable Trait: add insert(),search(),delete() functions

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/traitstructures/HashTable.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/traitstructures/HashTable.scala
@@ -1,5 +1,8 @@
 package com.local.AlgoWithDataStructure.Data_Structure.traitstructures
 
-trait HashTable {
-  
+trait HashTable[Key,Value] {
+
+  def insert(key:Key,value:Value)
+  def search(key:Key):Option[Value]
+  def delete(key:Key):Option[Value]
 }

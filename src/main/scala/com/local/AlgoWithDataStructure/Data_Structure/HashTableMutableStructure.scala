09b51faa03733743c37cb8e760e29d07c2e9b362
Raymond Yassin <ryassin92@gmail.com>
2020-05-19 22:05:20 -0400

    AlgoWithDataStructure HashTable Trait: refactor insert() to return unit

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/HashTableMutableStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/HashTableMutableStructure.scala
@@ -1,5 +1,11 @@
 package com.local.AlgoWithDataStructure.Data_Structure
+import com.local.AlgoWithDataStructure.Data_Structure.traitstructures.HashTable
 
-class HashTableMutableStructure[Key,Value](size:Int){
+class HashTableMutableStructure[Key,Value](size:Int) extends HashTable[Key,Value]{
+
+  def insert(key:Key,value:Value)
+
+  def search(key:Key):Option[Value]
+  def delete(key:Key):Option[Value]
 
 }
--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/traitstructures/HashTable.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/traitstructures/HashTable.scala
@@ -2,7 +2,7 @@ package com.local.AlgoWithDataStructure.Data_Structure.traitstructures
 
 trait HashTable[Key,Value] {
 
-  def insert(key:Key,value:Value)
+  def insert(key:Key,value:Value):Unit
   def search(key:Key):Option[Value]
   def delete(key:Key):Option[Value]
 }

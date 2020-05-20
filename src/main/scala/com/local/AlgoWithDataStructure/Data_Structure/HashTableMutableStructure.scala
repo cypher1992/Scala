3b806df5e98a4384ed8f33d272103aecc3b58756
Raymond Yassin <ryassin92@gmail.com>
2020-05-19 22:09:29 -0400

    AlgoWithDataStructure HashTableMutableStructure: add attribute myhashArray

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/HashTableMutableStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/HashTableMutableStructure.scala
@@ -3,9 +3,14 @@ import com.local.AlgoWithDataStructure.Data_Structure.traitstructures.HashTable
 
 class HashTableMutableStructure[Key,Value](size:Int) extends HashTable[Key,Value]{
 
-  def insert(key:Key,value:Value)
+  private val myHashArray = Array.fill(size)(List[Key,Value]())
+
+  
+
+  /*
+  def insert(key:Key,value:Value):Unit
 
   def search(key:Key):Option[Value]
-  def delete(key:Key):Option[Value]
+  def delete(key:Key):Option[Value]*/
 
 }

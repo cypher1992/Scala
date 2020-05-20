5348e06075fca2a4c5e13b530c71077546dee6f2
Raymond Yassin <ryassin92@gmail.com>
2020-05-19 22:13:39 -0400

    AlgoWithDataStructure HashTableMutableStructure: add hashCode(key) returns Int

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/HashTableMutableStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/HashTableMutableStructure.scala
@@ -5,7 +5,15 @@ class HashTableMutableStructure[Key,Value](size:Int) extends HashTable[Key,Value
 
   private val myHashArray = Array.fill(size)(List[Key,Value]())
 
-  
+  def hashCode[Key](mykey:Key):Int = {
+    val tempHashCode:Int = mykey.## % size
+    if(tempHashCode <0){
+      tempHashCode + size
+    }else{
+      tempHashCode
+    }
+  }
+
 
   /*
   def insert(key:Key,value:Value):Unit

c0e811af013d4efdaa738efae7887be509868572
Raymond Yassin <ryassin92@gmail.com>
2020-05-24 16:55:05 -0400

    AlgoAndDataStructure HashTableMutableStructure: test ifInstanceOfHashTable is true

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/HashTableTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/HashTableTest.scala
@@ -1,14 +1,21 @@
 package com.AlgoAndDataStructureTest.ArrayStructureTest
 
 import com.local.AlgoWithDataStructure.Data_Structure.HashTableMutableStructure
+import com.local.AlgoWithDataStructure.Data_Structure.traitstructures.HashTable
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
 class HashTableTest extends AnyFlatSpec with Matchers{
 
-  "HashTableMutableStructure: isInstanceOfHashTable" should "return true" in {
+  "HashTableMutableStructure: isInstanceOfMutableHashTable" should "return true" in {
     val htms:HashTableMutableStructure[Int,String] = new HashTableMutableStructure[Int,String](20)
     val isHashTableMutableStructure:Boolean = htms.isInstanceOf[HashTableMutableStructure[Int,String]]
+    assert(isHashTableMutableStructure == true)
+  }
+
+  "HashTableMutableStructure: isInstanceOfHashTable" should "return true" in {
+    val htms:HashTableMutableStructure[Int,String] = new HashTableMutableStructure[Int,String](20)
+    val isHashTableMutableStructure:Boolean = htms.isInstanceOf[HashTable[Int,String]]
     assert(isHashTableMutableStructure == true)
   }
 

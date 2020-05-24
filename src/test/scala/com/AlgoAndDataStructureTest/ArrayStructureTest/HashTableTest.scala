659cde3a8ff88309bcef2762049e208222d1cb56
Raymond Yassin <ryassin92@gmail.com>
2020-05-24 16:46:51 -0400

    AlgoAndDataStructure HashTableMutableStructure: creating test class

---
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/HashTableTest.scala
@@ -0,0 +1,10 @@
+package com.AlgoAndDataStructureTest.ArrayStructureTest
+
+import com.local.AlgoWithDataStructure.Data_Structure.HashTableMutableStructure
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+
+class HashTableTest extends AnyFlatSpec with Matchers{
+  
+
+}

d17a14fb9a1b39ea66ac8cb62a10553fc9bbe959
Raymond Yassin <ryassin92@gmail.com>
2020-05-28 22:39:51 -0400

    Scala BinaryTree: add case class Branch

---
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/Branch.scala
@@ -0,0 +1,6 @@
+package com.local.AlgoWithDataStructure.Data_Structure
+import com.local.AlgoWithDataStructure.Data_Structure.traitstructures.BinaryTree
+
+case class Branch[A](value:A,left:BinaryTree[A],right:BinaryTree[A]) extends BinaryTree[A] {
+  
+}

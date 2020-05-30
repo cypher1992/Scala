8032adb7723b97df7129fce93359efca5c257ce2
Raymond Yassin <ryassin92@gmail.com>
2020-05-29 23:09:01 -0400

    Scala BinaryTree: add Tree class

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/BinaryTreeMod/Branch.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/BinaryTreeMod/Branch.scala
@@ -1,6 +1,7 @@
-package com.local.AlgoWithDataStructure.Data_Structure
+package com.local.AlgoWithDataStructure.Data_Structure.BinaryTreeMod
+
 import com.local.AlgoWithDataStructure.Data_Structure.traitstructures.BinaryTree
 
 case class Branch[A](value:A,left:BinaryTree[A],right:BinaryTree[A]) extends BinaryTree[A] {
-  
+
 }
---
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/BinaryTreeMod/Tree.scala
@@ -0,0 +1,10 @@
+package com.local.AlgoWithDataStructure.Data_Structure.BinaryTreeMod
+
+import com.local.AlgoWithDataStructure.Data_Structure.traitstructures.BinaryTree
+import com.local.AlgoWithDataStructure.Data_Structure.BinaryTreeMod.Branch
+import com.local.AlgoWithDataStructure.Data_Structure.BinaryTreeMod.Leaf
+
+class Tree {
+
+
+}

d5b49e3b82ef103c3b5f9a5322758c60947dfaec
Raymond Yassin <ryassin92@gmail.com>
2020-05-31 22:14:47 -0400

    Scala BinaryTree: test isInstanceOfBinaryTree is true

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/TreeTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/TreeTest.scala
@@ -7,11 +7,18 @@ import org.scalatest.matchers.must.Matchers
 
 class TreeTest extends AnyFlatSpec with Matchers{
 
-"Tree: test instanceOf Tree" should "return True" in {
-  val tree:Tree = new Tree()
-  val isInstanceOfTree:Boolean = tree.isInstanceOf[Tree]
-  assert(isInstanceOfTree === true)
-}
+  "Tree: test instanceOf Tree" should "return True" in {
+    val tree:Tree = new Tree()
+    val isInstanceOfTree:Boolean = tree.isInstanceOf[Tree]
+    assert(isInstanceOfTree === true)
+  }
 
+  "Tree: test instanceOf BinaryTree" should "return True" in {
+    val tree:Tree = new Tree()
+    val list:List[Int] = List(1,2,5,6,7,10)
+    val bt:BinaryTree[Int] = tree.init_Tree(list)
+    val isInstanceOfTree:Boolean = bt.isInstanceOf[BinaryTree[Int]]
+    assert(isInstanceOfTree === true)
+  }
 
 }

d8c4a852ea88fc8b8de0eb2ff9929dbd8f9af882
Raymond Yassin <ryassin92@gmail.com>
2020-06-02 22:11:12 -0400

    Scala BinaryTree: test depth(List()) returns 0

--- src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/TreeTest.scala
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/TreeTest.scala
@@ -64,5 +64,14 @@ class TreeTest extends AnyFlatSpec with Matchers{
     val expected:Int = 0
     assert(expected === actual)
   }
+  
+  "Tree: test depth(list())" should "return 0" in {
+    val tree:Tree = new Tree()
+    val list:List[Int] = List.empty[Int]
+    val bt:BinaryTree[Int] = tree.init_Tree(list)
+    val actual:Int = tree.depth(bt)
+    val expected:Int = 0
+    assert(expected === actual)
+  }
 
 }

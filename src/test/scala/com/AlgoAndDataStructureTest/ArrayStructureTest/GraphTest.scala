51bd51c7e2cdf44ce40ded14743d7a8ec8865630
Raymond Yassin <ryassin92@gmail.com>
2020-06-04 21:47:54 -0400

    AlgoAndDataStructure Graph: Add GraphTest class

---
+++ src/test/scala/com/AlgoAndDataStructureTest/ArrayStructureTest/GraphTest.scala
@@ -0,0 +1,10 @@
+package com.AlgoAndDataStructureTest.ArrayStructureTest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import com.local.AlgoWithDataStructure.Data_Structure.GraphStructure
+
+class GraphTest extends AnyFlatSpec with Matchers{
+  
+
+}

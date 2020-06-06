400c105b705f3f792979fb9bd4059f99bc062b43
Raymond Yassin <ryassin92@gmail.com>
2020-06-05 22:05:15 -0400

    PowerApp : add calcSuccessor(vertex, graph)

--- src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/GraphStructure.scala
+++ src/main/scala/com/local/AlgoWithDataStructure/Data_Structure/GraphStructure.scala
@@ -4,4 +4,12 @@ class GraphStructure[T](list:List[Tuple2[T,T]]){
 
   def getList():List[Tuple2[T,T]] = this.list
 
+  def calcSuccessorSet(vertex:T,graph:List[Tuple2[T,T]]=this.getList()):List[T] = {
+    graph match {
+      case Nil => Nil
+      case x :: xs if(vertex == x._1) => x._2 ::  calcSuccessorSet(vertex,xs)
+      case _ :: xs =>   calcSuccessorSet(vertex,xs)
+    }
+  }
+
 }

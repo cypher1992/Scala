dc38afa8d1cc4bd98c74319dc892c6aab6291f2e
Raymond Yassin <ryassin92@gmail.com>
2020-06-12 22:57:30 -0400

    Art of Scala Types: add initBool(),initUnit(), initChar(), initAnyRef(),initString,InitAnyVal()

--- src/main/scala/com/ArtOfScala/Basics/Types.scala
+++ src/main/scala/com/ArtOfScala/Basics/Types.scala
@@ -2,13 +2,20 @@ package com.ArtOfScala.Basics
 
 class Types {
 
+  def initAnyVal(anyval:AnyVal):AnyVal = anyval
   def initDouble(double:Double):Double = double
   def initFloat(float:Float):Float = float
   def initInteger(int:Integer):Integer = int
   def initLong(long:Long):Long= long
   def initShort(short:Short):Short = short
   def initByte(byte:Byte):Byte = byte
-  
+  def initUnit():Unit = println("Unit is similar to void. Does nothing")
+  def initBool(bool:Boolean):Boolean = bool
+  def initChar(char:Char):Char = char
+
+  def initAnyRef(anyref:AnyRef):AnyRef = anyref
+  def initString(string:String):String = string
+
 
 
 

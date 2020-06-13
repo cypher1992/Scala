19e6ab11c907531030427b90f1059ae5d6234bb0
Raymond Yassin <ryassin92@gmail.com>
2020-06-12 22:50:19 -0400

    Art of Scala Types: add initDouble(), initFloat(), initInteger(, initLong(), initShort(), initByte())

---
+++ src/main/scala/com/ArtOfScala/Basics/Types.scala
@@ -0,0 +1,15 @@
+package com.ArtOfScala.Basics
+
+class Types {
+
+  def initDouble(double:Double):Double = double
+  def initFloat(float:Float):Float = float
+  def initInteger(int:Integer):Integer = int
+  def initLong(long:Long):Long= long
+  def initShort(short:Short):Short = short
+  def initByte(byte:Byte):Byte = byte
+  
+
+
+
+}

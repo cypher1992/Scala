c2a8b39ea978f5b7c8634ae3e1f338a5a307e3aa
Raymond Yassin <ryassin92@gmail.com>
2020-06-14 22:32:11 -0400

    Art of Scala Conditional: add ifElseEqualsConditional(value:T, testValue:T)

--- src/main/scala/com/ArtOfScala/Basics/Conditonal.scala
+++ src/main/scala/com/ArtOfScala/Basics/Conditonal.scala
@@ -1,6 +1,15 @@
 package com.ArtOfScala.Basics
 
 class Conditonal[T]{
+
+  def ifElseEqualsCondtion(value:T,testValue:T):Boolean ={
+    if(value == testValue){
+      true
+    }else{
+      false
+    }
+  }
+
   
 
 }

18e70c0ab612697d9e02df497e20dfba461e6677
Raymond Yassin <ryassin92@gmail.com>
2020-06-15 22:14:33 -0400

    Art of Scala Conditional: test ifElseIfElseEqualsConditional(value:T, testValue:T, testValue1:T) returns true/false

--- src/test/scala/com/ArtOfScalaTDD/Basics/ConditionalTest/ConditonalTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/ConditionalTest/ConditonalTest.scala
@@ -20,4 +20,25 @@ class ConditonalTest extends AnyFlatSpec with Matchers{
     assert(actual == expected)
   }
 
+
+  "ArtOfScala Basics Conditional: ifElseEqualsCondtion(Int 3, Int 4, Int 5)" should "return false" in {
+    val conditional:Conditonal[Int] = new Conditonal[Int]
+    val actual:Boolean = conditional.ifElseIfElseEqualsCondtion(3,4,5)
+    val expected:Boolean = false
+    assert(actual == expected)
+  }
+
+  "ArtOfScala Basics Conditional: ifElseEqualsCondtion(Int 3, Int 3, Int 5)" should "return true" in {
+    val conditional:Conditonal[Int] = new Conditonal[Int]
+    val actual:Boolean = conditional.ifElseIfElseEqualsCondtion(3,3,5)
+    val expected:Boolean = true
+    assert(actual == expected)
+  }
+
+  "ArtOfScala Basics Conditional: ifElseEqualsCondtion(Int 3, Int 4, Int 3)" should "return true" in {
+    val conditional:Conditonal[Int] = new Conditonal[Int]
+    val actual:Boolean = conditional.ifElseIfElseEqualsCondtion(3,4,3)
+    val expected:Boolean = true
+    assert(actual == expected)
+  }
 }

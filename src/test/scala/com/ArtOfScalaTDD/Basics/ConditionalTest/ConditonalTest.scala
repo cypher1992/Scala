71fbdbe371da1c5d302739c72170c8b0c17bcddd
Raymond Yassin <ryassin92@gmail.com>
2020-06-15 22:09:33 -0400

    Art of Scala Conditional: add test ContionalTest Class

---
+++ src/test/scala/com/ArtOfScalaTDD/Basics/ConditionalTest/ConditonalTest.scala
@@ -0,0 +1,10 @@
+package com.ArtOfScalaTDD.Basics.ConditionalTest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import com.ArtOfScala.Basics.Conditonal
+
+class ConditonalTest extends AnyFlatSpec with Matchers{
+  
+
+}

d5aed8390bdac95e6038ea7082d1ecf3b3dad632
Raymond Yassin <ryassin92@gmail.com>
2020-06-16 22:40:17 -0400

    Art of Scala Function: add(double,double2) returns sum(d+d2)

---
+++ src/main/scala/com/ArtOfScala/Basics/Function.scala
@@ -0,0 +1,10 @@
+package com.ArtOfScala.Basics
+
+class Function{
+
+  // basic function it has name
+  // parameter(s)
+  // return value(s)
+  def add(double:Double,double2:Double):Double = double+double2
+
+}

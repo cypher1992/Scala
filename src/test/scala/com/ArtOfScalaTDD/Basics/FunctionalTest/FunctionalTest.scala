6b710e73ca973ab1dd342a5ca524c5742922d915
Raymond Yassin <ryassin92@gmail.com>
2020-06-17 21:07:12 -0400

    Art of Scala Function: test add(3.0,4.0) returns 7

---
+++ src/test/scala/com/ArtOfScalaTDD/Basics/FunctionalTest/FunctionalTest.scala
@@ -0,0 +1,16 @@
+package com.ArtOfScalaTDD.Basics.FunctionalTest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+import com.ArtOfScala.Basics.Function
+
+class FunctionalTest extends AnyFlatSpec with Matchers {
+
+  "ArtOfScala Basics Functional: add(Double 3.0, Double 4.0)" should "return 7" in {
+    val function:Function = new Function()
+    val actual:Double = function.add(3.0,4.0)
+    val expected:Double = 7
+    assert(actual == expected)
+  }
+
+}

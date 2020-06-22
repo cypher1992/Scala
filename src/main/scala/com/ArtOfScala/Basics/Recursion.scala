e6ec9220f311795ff91de97b22ccb17a7b0e020b
Raymond Yassin <ryassin92@gmail.com>
2020-06-21 22:41:55 -0400

    Art of Scala Recursive: add multiplyItselfCon() returns Int

--- src/main/scala/com/ArtOfScala/Basics/Recursion.scala
+++ src/main/scala/com/ArtOfScala/Basics/Recursion.scala
@@ -4,6 +4,7 @@ class Recursion {
 
   // recursive function is primarily calling it self until it hits a satisfying condition. Almost like a while loop.
 
+  // using matching
   def multiplyItSelf(num:Int):Int = {
 
     def recurMulti(number:Int = num, store:Int = 1):Int = {
@@ -13,6 +14,22 @@ class Recursion {
         case _ => recurMulti(number-1,store*number)
       }
     }
+    recurMulti()
+  }
+
+  //using if and else conditions
+  def multiplyItSelfCon(num:Int):Int = {
+
+    def recurMulti(number:Int=num, store:Int = 1):Int ={
+      if(number == 0){
+        0
+      }else if(number == 1){
+        store
+      }else{
+        recurMulti(number-1,store*number)
+      }
+    }
+
     recurMulti()
   }
 

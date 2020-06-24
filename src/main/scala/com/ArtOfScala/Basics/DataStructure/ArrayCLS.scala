61f486a862f871f9c732dee0fce4fe51d2869fce
Raymond Yassin <ryassin92@gmail.com>
2020-06-23 22:12:04 -0400

    Art of Scala ArrayCls: add setArr(newArr:Array[T]) returns Unit

--- src/main/scala/com/ArtOfScala/Basics/DataStructure/ArrayCLS.scala
+++ src/main/scala/com/ArtOfScala/Basics/DataStructure/ArrayCLS.scala
@@ -3,6 +3,6 @@ package com.ArtOfScala.Basics.DataStructure
 class ArrayCLS[T](var arr:Array[T]){
 
     def getArr():Array[T] = this.arr
-  
+    def setArr(newArr:Array[T]):Unit = this.arr = newArr
 
 }

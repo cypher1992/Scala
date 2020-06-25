09c318ced138701b301048bf17a7eb7984e31d4c
Raymond Yassin <ryassin92@gmail.com>
2020-06-24 22:39:33 -0400

    Art of Scala ArrayCls: test getArr() return arry

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -6,9 +6,17 @@ import com.ArtOfScala.Basics.DataStructure.ArrayCLS
 
 class ArrayClsTest extends AnyFlatSpec with Matchers{
 
-  "ArtOfScala ArrayCls: test instanceOfArrayCls" should "Return true" in {
+  "ArtOfScala ArrayCls: instanceOfArrayCls" should "Return true" in {
     val array:Array[Int] = new Array[Int](3)
     val arrCls:ArrayCLS[Int] = new ArrayCLS(array)
     assert( arrCls.isInstanceOf[ArrayCLS[Int]] )
+  }
+
+  "ArtOfScala ArrayCls: getArr()" should "return EmptyArray[Int]" in {
+    val array: Array[Int] = new Array[Int](3)
+    val arrCls: ArrayCLS[Int] = new ArrayCLS(array)
+    val expected: Array[Int] = new Array[Int](3)
+    val actual: Array[Int] = arrCls.getArr()
+    assert(expected === actual)
   }
 }

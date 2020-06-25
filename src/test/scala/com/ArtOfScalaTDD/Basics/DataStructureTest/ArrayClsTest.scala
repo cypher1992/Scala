57904fcc407513ea4fe77b6418f3d468ac1b5787
Raymond Yassin <ryassin92@gmail.com>
2020-06-24 22:45:54 -0400

    Art of Scala ArrayCls: test setArr(newArray) return Unit

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -19,4 +19,14 @@ class ArrayClsTest extends AnyFlatSpec with Matchers{
     val actual: Array[Int] = arrCls.getArr()
     assert(expected === actual)
   }
+
+  "ArtOfScala ArrayCls: setArr(newArry)" should "return Array(3)" in {
+    val array: Array[Int] = new Array[Int](3)
+    val arrCls: ArrayCLS[Int] = new ArrayCLS(array)
+    val expected: Array[Int] =  Array[Int](3)
+    val newArray: Array[Int] =  Array[Int](3)
+    arrCls.setArr(newArray)
+    val actual: Array[Int] = arrCls.getArr()
+    assert(expected === actual)
+  }
 }

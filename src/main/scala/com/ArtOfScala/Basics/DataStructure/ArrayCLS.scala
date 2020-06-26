51f8d951d8b43c3a9327bdd8c228f7e7c364ed10
Raymond Yassin <ryassin92@gmail.com>
2020-06-25 21:46:41 -0400

    Art of Scala ArrayCls: add addIndex(obj,index)

--- src/main/scala/com/ArtOfScala/Basics/DataStructure/ArrayCLS.scala
+++ src/main/scala/com/ArtOfScala/Basics/DataStructure/ArrayCLS.scala
@@ -5,4 +5,10 @@ class ArrayCLS[T](var arr:Array[T]){
     def getArr():Array[T] = this.arr
     def setArr(newArr:Array[T]):Unit = this.arr = newArr
 
+    def addIndex(obj:T,index:Int):Unit = {
+      if(index < this.getArr().size && index > -1 ) {
+        this.getArr()(index) = obj
+      }
+    }
+
 }

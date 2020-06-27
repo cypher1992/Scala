c99ff3e12c4592704ebdc8edd3fc12ff56e812b9
Raymond Yassin <ryassin92@gmail.com>
2020-06-26 23:07:29 -0400

    Art of Scala ArrayCls: test addIndex(2,1)

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -57,12 +57,21 @@ class ArrayClsTest extends AnyFlatSpec with Matchers{
     assert(expected === actual)
   }
 
-  "ArtOfScala ArrayCls: addIndex(2,2)" should "return Array(0,0,0)" in {
+  "ArtOfScala ArrayCls: addIndex(2,2)" should "return Array(0,0,2)" in {
     val array: Array[Int] = new Array[Int](3)
     val arrCls: ArrayCLS[Int] = new ArrayCLS(array)
     arrCls.addIndex(2,2)
     val actual:Array[Int] = arrCls.getArr()
     val expected:Array[Int] = Array(0,0,2)
+    assert(expected === actual)
+  }
+
+  "ArtOfScala ArrayCls: addIndex(2,1)" should "return Array(0,2,0)" in {
+    val array: Array[Int] = new Array[Int](3)
+    val arrCls: ArrayCLS[Int] = new ArrayCLS(array)
+    arrCls.addIndex(2,1)
+    val actual:Array[Int] = arrCls.getArr()
+    val expected:Array[Int] = Array(0,2,0)
     assert(expected === actual)
   }
 

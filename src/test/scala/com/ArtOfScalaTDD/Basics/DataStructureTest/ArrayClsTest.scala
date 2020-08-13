07095e46761e03bfdad3bbaf89d6ea3f14130d8f
Raymond Yassin <ryassin92@gmail.com>
2020-08-12 21:23:30 -0400

    SCALA101 ART OF SCALA - ArrayCls: refactor array.map(lessThan10) array(5) WIP

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -810,5 +810,16 @@ class ArrayClsTest extends AnyFlatSpec with Matchers{
     assert(actual === expected)
   }
 
+  "ArtOfScala ArrayCls: array.map(lessthan10)" should "array(5)" in {
+    val array: Array[Int] = Array(5,10)
+    val arr: ArrayCLS[Int] = new ArrayCLS(array)
+    def lessThan10(int: Int): Int = int match {
+      case a if(a<10) => a
+    }
+    val actual: Array[Int] = arr.mapX(lessThan10)
+    val expected: Array[Int] = Array(5)
+    assert(actual === expected)
+  }
+
 
 }

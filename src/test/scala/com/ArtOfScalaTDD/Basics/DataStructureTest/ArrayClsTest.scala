5319aa62bedb0d520c907c27e17c55eaf906a1a9
Raymond Yassin <ryassin92@gmail.com>
2020-08-13 21:43:22 -0400

    SCALA101 ART OF SCALA - ArrayCls: test arr.partitionX returns Array(1) as first tuple

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -808,5 +808,12 @@ class ArrayClsTest extends AnyFlatSpec with Matchers{
     assert(actual === expected)
   }
 
-
+  "ArtOfScala ArrayCls: array.partionX(lessThan10)" should "Array(1)" in {
+    val array: Array[Int] = Array(1,10)
+    val arr: ArrayCLS[Int] = new ArrayCLS(array)
+    def lessThan10(int: Int):Boolean = if(int< 10) true else false
+    val actual: Array[Int] = arr.partionX(lessThan10)._1
+    val expected: Array[Int] = Array(1)
+    assert(actual === expected)
+  }
 }

793b87bfcbb255628dc7898d763ea9805c5d2e2d
Raymond Yassin <ryassin92@gmail.com>
2020-08-14 23:28:58 -0400

    SCALA101 ART OF SCALA - ArrayCls: test arr.partitionX return array(1,1,) as first tuple

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -826,12 +826,21 @@ class ArrayClsTest extends AnyFlatSpec with Matchers{
     assert(actual === expected)
   }
 
-  "ArtOfScala ArrayCls: array.partionX(lessThan10)" should "Second Tuple return Array(1)" in {
+  "ArtOfScala ArrayCls: array.partionX(lessThan10)" should "Second Tuple return Array(10)" in {
     val array: Array[Int] = Array(1,10)
     val arr: ArrayCLS[Int] = new ArrayCLS(array)
     def lessThan10(int: Int):Boolean = if(int< 10) true else false
     val actual: Array[Int] = arr.partionX(lessThan10)._2
     val expected: Array[Int] = Array(10)
+    assert(actual === expected)
+  }
+
+  "ArtOfScala ArrayCls: array.partionX(lessThan10)" should "Second Tuple return Array(1,1)" in {
+    val array: Array[Int] = Array(1,10,1)
+    val arr: ArrayCLS[Int] = new ArrayCLS(array)
+    def lessThan10(int: Int):Boolean = if(int< 10) true else false
+    val actual: Array[Int] = arr.partionX(lessThan10)._1
+    val expected: Array[Int] = Array(1,1)
     assert(actual === expected)
   }
 }

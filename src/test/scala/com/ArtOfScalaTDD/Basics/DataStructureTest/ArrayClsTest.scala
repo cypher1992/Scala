f1131eb2753f73f2f888679fb7b9d5da9e4a1723
Raymond Yassin <ryassin92@gmail.com>
2020-08-28 23:00:07 -0400

    SCALA101 ART OF SCALA - ArrayCls: refactor emptyArray.combination(1) returns List()

--- src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
+++ src/test/scala/com/ArtOfScalaTDD/Basics/DataStructureTest/ArrayClsTest.scala
@@ -1061,6 +1061,19 @@ class ArrayClsTest extends AnyFlatSpec with Matchers{
     assert(actual === expected)
   }
 
+  "ArtOfScala ArrayCls: emptyarray.combinationX(1)" should "return empty combination" in {
+    val array: Array[Int] = Array.empty[Int]
+    val arr: ArrayCLS[Int] = new ArrayCLS(array)
+    val iteratorArray: Iterator[Array[Int]] = arr.combinationX(1)
+    var actual:List[String] = List.empty[String]
+    for( i <- iteratorArray){
+      actual = actual.appended(i.mkString)
+    }
+    println(actual)
+    val expected: List[String] = List()
+    assert(actual === expected)
+  }
+
   "ArtOfScala ArrayCls: array.combinationX(1)" should "return empty combination" in {
     val array: Array[Int] = Array(1,0)
     val arr: ArrayCLS[Int] = new ArrayCLS(array)

3a8f35562bc89e7dd5c985cf4718b0617274aee4
cypher1992 <ryassin92@gmail.com>
2021-01-09 20:57:25 -0500

    SCALAREFRESHER:VariableAndType: define a val

---
+++ src/main/scala/com/refresher/basics/VariablesAndTypes.scala
@@ -0,0 +1,16 @@
+package com.refresher.basics
+
+class VariablesAndTypes {
+
+  /*
+  Variables is the bread and butter of a programming. Immutability is first class citizen in scala programming.
+    - Define variables using key word val
+    eg.
+    - val variableName:DataType = assignedValue
+   */
+
+  val meetValTheVariable:Int = 5
+
+
+
+}

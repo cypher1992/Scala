45b50f3fda4c5438223dd43710185ba7731596a8
cypher1992 <ryassin92@gmail.com>
2021-01-12 23:11:33 -0500

    SCALA:REVIEW:CONDITIONSTRUCTURES: add variables regarding to private and public accessors and constructor)

---
+++ src/main/scala/com/refresher/basics/ConditionStructures.scala
@@ -0,0 +1,15 @@
+package com.refresher.basics
+
+class ConditionStructures(private val number:Int){
+
+  private var isPositive:Boolean = false
+  private var isNegative:Boolean = false
+  private var isNeutral:Boolean = false
+
+  def getNumber():Int = this.number
+
+
+
+
+
+}

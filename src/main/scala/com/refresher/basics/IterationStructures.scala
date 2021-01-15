0a277a7d189d85fd8e658b27a402486642bc6809
cypher1992 <ryassin92@gmail.com>
2021-01-14 21:15:33 -0500

    SCALA:REVIEW:ITERATIONSTRUCTURES: refactor class

--- src/main/scala/com/refresher/basics/IterationStructures.scala
+++ src/main/scala/com/refresher/basics/IterationStructures.scala
@@ -4,36 +4,43 @@ class IterationStructures(private val start:Int,private val end:Int) {
 
   private var counter:Int =0
 
+  def setCounter(newCounter:Int):Unit = this.counter = newCounter
+  def getCounter():Int = this.counter
+
   def forToLoop():Int = {
     //this iterate up to end
     for(counter <- start to end){
-      println(counter)
+      println(this.counter)
+      this.setCounter(counter)
     }
-    counter
+    this.getCounter()
   }
 
   def forUntilLoop():Int = {
     // this iterate up to end-1
     for(counter <- start until end){
       println(counter)
+      this.setCounter(counter)
     }
-    counter
+    this.getCounter()
   }
 
   def whileLoop(): Int ={
     while(counter<end){
       println(counter)
       counter+=1
+      this.setCounter(counter)
     }
-    counter
+    this.getCounter()
   }
 
   def doWhileLoop():Int ={
     do{
       println(counter)
       counter+=1
+      this.setCounter(counter)
     }while(counter<end)
-    counter
+    this.getCounter()
   }
 
 }

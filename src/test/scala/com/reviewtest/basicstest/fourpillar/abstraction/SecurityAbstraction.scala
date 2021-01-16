fa629c00e4d5f5b128bb33fa5535c817c6cf3eb1
cypher1992 <ryassin92@gmail.com>
2021-01-15 22:01:47 -0500

    SCALA:FOURPILLAR:ABSTRACTION: add SecurityAbstraction)

--- src/test/scala/com/reviewtest/basicstest/fourpillar/abstraction/SecurityAbstraction.scala
+++
@@ -1,8 +0,0 @@
-package com.reviewtest.basicstest.fourpillar.abstraction
-
-abstract class SecurityAbstraction(private val ticker:String, private val exchangeTraded:String) {
-
-  def getTicker:String = this.ticker
-  def getExchangeTraded:String = this.exchangeTraded
-
-}

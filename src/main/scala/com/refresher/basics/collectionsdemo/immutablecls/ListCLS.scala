a6f94ba3a2d717573b56bcc7a2262567735e2764
cypher1992 <ryassin92@gmail.com>
2021-01-19 21:52:24 -0500

    SCALA:IMMUTABLE COLLECTIONS: define list as emptylist

--- src/main/scala/com/refresher/basics/collectionsdemo/immutablecls/ListCLS.scala
+++ src/main/scala/com/refresher/basics/collectionsdemo/immutablecls/ListCLS.scala
@@ -1,14 +1,13 @@
 package com.refresher.basics.collectionsdemo.immutablecls
-
 /*
 * Immutable List are list that cannot be changed
 * */
 
-class ListCLS(list:List[Int]){
+class ListCLS(list:List[Int]= List.empty[Int]){
 
   def get(index:Int):Int = this.list(index)
   def append(value:Int,list:List[Int]):List[Int] = list :+ value
   def prepend(value:Int,list:List[Int]):List[Int] = value +: list
-  def appendList(newList:List[Int],list:List[Int]):List[Int] = list :+ newList
+  def appendList(newList:List[Int],list:List[Int]):List[Any] = list :: newList
 
 }

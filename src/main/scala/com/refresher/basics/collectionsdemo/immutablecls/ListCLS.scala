3dc37467d127847c7a3c85b7fee5a76468598204
cypher1992 <ryassin92@gmail.com>
2021-01-20 22:37:09 -0500

    SCALA:IMMUTABLE COLLECTIONS:LISTCLS: test list.append(1)

--- src/main/scala/com/refresher/basics/collectionsdemo/immutablecls/ListCLS.scala
+++ src/main/scala/com/refresher/basics/collectionsdemo/immutablecls/ListCLS.scala
@@ -3,11 +3,12 @@ package com.refresher.basics.collectionsdemo.immutablecls
 * Immutable List are list that cannot be changed
 * */
 
-class ListCLS(list:List[Int]= List.empty[Int]){
+class ListCLS(private val list:List[Int]= List.empty[Int]){
 
+  def getList():List[Int] = this.list
   def get(index:Int):Int = this.list(index)
-  def append(value:Int,list:List[Int]):List[Int] = list :+ value
-  def prepend(value:Int,list:List[Int]):List[Int] = value +: list
-  def appendList(newList:List[Int],list:List[Int]):List[Any] = list :: newList
+  def append(value:Int,list:List[Int]=list):List[Int] = list :+ value
+  def prepend(value:Int,list:List[Int]=list):List[Int] = value +: list
+  def appendList(newList:List[Int],list:List[Int]=list):List[Any] = list :: newList
 
 }
